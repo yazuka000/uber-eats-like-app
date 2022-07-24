b5d24c27234b41f1e949f42eb731fb6a04a338b7
yazuka000 <yazuka.000@gmail.com>
2022-07-24 14:55:24 +0900

    successed api function

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,5 +1,11 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
+
+import { fetchRestaurants } from "../apis/restaurants";
 
 export const Restaurants = () => {
+  useEffect(() => {
+    fetchRestaurants().then((data) => console.log(data));
+  }, []);
+
   return <Fragment>レストラン一覧</Fragment>;
 };
