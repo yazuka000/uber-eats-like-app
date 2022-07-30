9d225be78de1777f0eea0044a9fe3eda94992910
yazuka000 <yazuka.000@gmail.com>
2022-07-30 10:05:47 +0900

    bug fixed in line_foods_controller.rb

--- backend/app/controllers/api/v1/line_foods_controller.rb
+++ backend/app/controllers/api/v1/line_foods_controller.rb
@@ -1,7 +1,7 @@
 module Api 
   module V1 
     class LineFoodsController < ApplicationController
-      before_action :set_food, only: %i[create]
+      before_action :set_food, only: %i[create replace]
 
       def index
         line_foods = LineFood.active
@@ -36,6 +36,22 @@ module Api
           render json: {}, status: :internal_server_error 
         end
       end
+
+      def replace
+        LineFood.active.other_restaurant(@ordered_food.restaurant.id).each do |line_food|
+          line_food.update_attribute(:active, false)
+        end
+
+        set_line_food(@ordered_food)
+
+        if @line_food.save
+          render json: {
+            line_food: @line_food
+          }, status: :created
+          else
+            render json: {}, status: :internal_server_error
+          end
+        end
 
       private
 
