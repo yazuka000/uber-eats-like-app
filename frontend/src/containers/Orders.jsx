79a585cde19707de81381fc29921b01851f9ac2b
yazuka000 <yazuka.000@gmail.com>
2022-07-31 10:37:30 +0900

    added fetchLineFoods api

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,5 +1,15 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
+
+import { fetchLineFoods } from "../apis/line_foods";
 
 export const Orders = () => {
+  useEffect(() => {
+    fetchLineFoods()
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
   return <Fragment>注文画面</Fragment>;
 };
