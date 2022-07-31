37ebefe868893a244077e96006ae7bbc113a38b4
yazuka000 <yazuka.000@gmail.com>
2022-07-31 10:52:45 +0900

    created lineFoodsReducer and modified Orders.jsx

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,12 +1,27 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useReducer } from "react";
 
 import { fetchLineFoods } from "../apis/line_foods";
 
+// reducers
+import {
+  initialState,
+  lineFoodsActionTypes,
+  lineFoodsReducer,
+} from "../reducers/lineFoods";
+
 export const Orders = () => {
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+
   useEffect(() => {
+    dispatch({ type: lineFoodsActionTypes.FETCHING });
     fetchLineFoods()
       .then((data) => {
-        console.log(data);
+        dispatch({
+          type: lineFoodsActionTypes.FETCH_SUCCESS,
+          payload: {
+            lineFoodsSummary: data, //
+          },
+        });
       })
       .catch((e) => console.log(e));
   }, []);
