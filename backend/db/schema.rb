905ebb0b93be10eba8bce56e3a9eed2c9a2bd2fd
yazuka000 <yazuka.000@gmail.com>
2022-08-02 10:35:34 +0900

    complete uber-eats-like-app

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_17_051753) do
+ActiveRecord::Schema.define(version: 2022_07_18_051740) do
 
   create_table "foods", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "restaurant_id", null: false
